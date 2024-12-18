2d4b12e82b61d6fb0e14963c4135181863452457
kieran-grant <kierang92@gmail.com>
2024-12-18 14:04:39 +0000

    Add not quite solved for part 2

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -15,34 +15,41 @@
 
 (def contents (parse filename))
 
-(defn zip-self-with
-  [fn list]
-  (map fn list (rest list)))
+(defn zip-with
+  [fn list-a list-b]
+  (map fn list-a list-b))
 
 (defn safely-increasing?
   "Checks whether the given list is monotonically increasing within the bound"
-  [list]
-  (every? #(and (pos? %) (<= % 3)) (zip-self-with - list)))
+  [item]
+  (and (pos? item) (<= item 3)))
 
 (defn safely-decreasing?
   "Checks whether the given list is monotonically decreasing within the bound"
-  [list]
-  (every? #(and (neg? %) (>= % -3)) (zip-self-with - list)))
+  [item]
+  (and (neg? item) (>= item -3)))
 
 (defn safe?
   [list]
-  (and (> (count list) 1)
-       (or (safely-increasing? list) (safely-decreasing? list))))
+  (let [zipped (zip-with - list (rest list))]
+    (and (> (count zipped) 0)
+         (or (every? safely-increasing?  zipped)
+             (every? safely-decreasing?  zipped)))))
+
+(defn kind-of-safe?
+  [list]
+  (let [zipped (zip-with - list (drop 2 list))]
+    (and (> (count zipped) 0)
+         (if (every? pos? zipped)
+           (<= (count (filter safely-increasing? zipped)) 1)
+           (<= (count (filter safely-decreasing? zipped)) 1)))))
 
 (def solution1 (count (filter safe? contents)))
 
-;; TODO: Part 2
-;; passes_explicitly = filter safe? [pair of (x, y)] 
-;; if (passes_explcitly == false) return filter safe? [pair of (x, z)] return len (filter safe? [paris of (x, z)]) <= 1; 
-;;
-;; return (or (passes_explicitly list) (kind-of-safe? list))
+(def solution2 (count (filter #(or (safe? %) (kind-of-safe? %)) contents)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& _args]
-  (println "Part 1 solution:" solution1)) ;; Use contents in -main if needed
+  (println "Part 1 solution:" solution1)
+  (println "Part 2 solution:" solution2)) ;; Use contents in -main if needed
