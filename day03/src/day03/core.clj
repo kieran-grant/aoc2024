c0fa6df43ebcbda87880959862952576ad439988
kieran-grant <kierang92@gmail.com>
2024-12-19 14:44:40 +0000

    day 3 - part 2 solved

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -1,5 +1,7 @@
 (ns day03.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.string]))
 
 (def regex #"mul\(([\d]+),([\d]+)\)")
 
@@ -11,7 +13,16 @@
 
 (def solution1 (reduce + (map multiply-pairs matches)))
 
+(def regex-dont-blocks #"(?x)don't\(\)[\s\S]*?(?=do\(\)|$)")
+
+(def input-without-dont (clojure.string/replace input regex-dont-blocks ""))
+
+(def matches-without-dont (map rest (re-seq regex input-without-dont)))
+
+(def solution2 (reduce + (map multiply-pairs matches-without-dont)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& _args]
-  (println "Part 1 solution: " solution1))
+  (println "Part 1 solution: " solution1)
+  (println "Part 2 solution: " solution2))
