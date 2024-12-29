a6b4833dba0486ed3f78c6a8771afb804aa05ac7
Kieran Grant <>
2024-12-29 19:33:13 +0000

    finish day 6 part 1

--- src/aoc2024/day06.clj
+++ src/aoc2024/day06.clj
@@ -25,26 +25,31 @@
   [grid]
   (first (for [[k v] grid :when (= v \^)] k)))
 
-(defn transpose
-  "Transposes a sequence of pairs into two sequences."
-  [pairs]
-  (apply map vector pairs))
-
-(defn get-bounds
-  [grid]
-  (let [[xs ys] (transpose (keys grid))]
-    [(apply max xs) (apply max ys)]))
-
 (def dirs {:north [0, -1]
            :east [1, 0]
            :south [0, 1]
            :west [-1, 0]})
 
-(find-start example-data)
-(get-bounds example-data)
+(defn turn-right
+  [dir]
+  (case dir
+    :north :east
+    :east :south
+    :south :west
+    :west :north))
 
 (defn move [curr dir]
   (map + curr (dirs dir)))
 
-(move [4,6] :north)
+(defn part-1
+  ([grid]
+   (part-1 grid (find-start grid) :north #{}))
+  ([grid curr-pos dir visited]
+   (let [new-visited (cons curr-pos visited)
+         new-pos (move curr-pos dir)]
+     (case (grid new-pos)
+       nil (count (set new-visited))
+       \# (recur grid curr-pos (turn-right dir) new-visited)
+       (recur grid new-pos dir new-visited)))))
 
+(part-1 data)
